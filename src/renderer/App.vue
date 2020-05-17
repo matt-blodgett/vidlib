34d726efbd841180c0e3b78d8e2e3d8b3fa5e0fb
Matthew <mattblodgett94@gmail.com>
2020-05-17 19:22:24 -0400

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vidlib'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
